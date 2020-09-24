5b2dc71a481df8ac130caef500be40c554dc307b
Nial George <71710710+nialgeorgeyt@users.noreply.github.com>
2020-09-24 14:03:33 +0530

    Add files via upload

---
+++ P79.js
@@ -0,0 +1,12 @@
+menu_list_items = ["Idly", "Papadum"];
+
+function run() {
+    document.getElementById("menu_list_items").innerHTML = menu_list_items;
+}
+
+function add_new_menu_item() {
+    new_menu_item = document.getElementById("new_menu_item").value;
+    menu_list_items.push(new_menu_item);
+    menu_list_items.sort();
+    document.getElementById("menu_list_items").innerHTML = menu_list_items;
+}
